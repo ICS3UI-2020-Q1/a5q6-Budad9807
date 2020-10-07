3fe820172956a8df1f519d093ed64f83e946b1f9
Budad9807 <Budad9807@wrdsb.ca>
2020-10-07 16:22:22 +0000

    I made a the program make a box

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * A program that goes through 1 to 10 and print a line under it
+ * @author David
  */
 public class Main {
 
@@ -9,7 +10,25 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // create a count variable
+    int count = 1;
+    // create a loop that goes up to 10 with a line
+    Scanner input = new Scanner(System.in);
+    System.out.println("Please enter a number between 1 to 10")
+    ;
+    int num = input.nextInt();
+    while(count <= num){
+      // now what ever number they enter will be print in lines
+      count = count + 1;
+      int count2 = 1;
+    while(count2 <= num){
+      System.out.print("*");
+      count2 = count2 + 1;
+    }
+     System.out.println();
+    }
+   
+
     
   }
 }
